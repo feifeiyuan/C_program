7e41ac4f1918f03dbe0e9ba1b001b79abfbbcdfa
feifeiyuan <33796779+feifeiyuan@users.noreply.github.com>
2018-04-12 20:09:25 +0800

    直接连接字符串无需函数

--- connect_double_str.c
+++ connect_double_str.c
@@ -4,7 +4,7 @@
 #include <stdarg.h>
 #include <sys/time.h>
 #define TRACE_STAT_FILE "/per_cpu/cpu0/stats"
-
+//还有就是使用strcat
 int main(void){
 	printf("%s\n","feifei_yuan""liu");
 	printf("%s\n","feifei_yuan"TRACE_STAT_FILE);
